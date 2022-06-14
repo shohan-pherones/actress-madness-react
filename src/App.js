69d1c0cb56877258e84adcac9e851a5d28a402e8
sterling#elysian.JP60=={56} <shohan.sub.56@gmail.com>
2022-06-15 01:52:33 +0600

    router added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
 import Header from "./components/Header/Header";
+import About from "./components/About/About";
+import Error from "./components/Error/Error";
 
 const App = () => {
   return (
     <div>
-      <Header></Header>
-      <Blogs></Blogs>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Blogs />} />
+          <Route exact path="/home" element={<Blogs />} />
+          <Route exact path="/blogs" element={<Blogs />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/*" element={<Error />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 };
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,35 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand className="logo">Actress Madness</Navbar.Brand>
+        <Navbar.Brand>
+          <Link to="/home" id="logo">
+            Actress Madness
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
-            <Nav.Link className="menu">Home</Nav.Link>
-            <Nav.Link className="menu">Blogs</Nav.Link>
-            <Nav.Link className="menu">About</Nav.Link>
+            <Nav.Link>
+              <Link to="/home" id="menu">
+                Home
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/blogs" id="menu">
+                Blogs
+              </Link>
+            </Nav.Link>
+            <Nav.Link>
+              <Link to="/about" id="menu">
+                About
+              </Link>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
